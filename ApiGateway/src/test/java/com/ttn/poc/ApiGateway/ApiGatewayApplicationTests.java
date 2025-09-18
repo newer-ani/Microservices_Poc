ec2472e748b001d08189d8e7923b23ecaac3ea3e
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-18 11:57:17 +0530

    added the api gateway

---
+++ ApiGateway/src/test/java/com/ttn/poc/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.poc.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- quizservice/src/main/java/com/ttn/quizservice/services/QuestionClient.java
+++ quizservice/src/main/java/com/ttn/quizservice/services/QuestionClient.java
@@ -2,7 +2,6 @@ package com.ttn.quizservice.services;
 
 import com.ttn.quizservice.entities.Question;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
