deb1bd679cd783c9532fc1464d85bda3956e9cb1
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-23 12:23:53 +0530

    Enable caching in main application classes
    - Added @EnableCaching annotation in QuestionServiceApplication and QuizServiceApplication

--- questionservice/src/main/java/com/ttn/questionservice/QuestionserviceApplication.java
+++ questionservice/src/main/java/com/ttn/questionservice/QuestionserviceApplication.java
@@ -2,8 +2,10 @@ package com.ttn.questionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class QuestionserviceApplication {
 
 	public static void main(String[] args) {
