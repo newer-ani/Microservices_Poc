1f479430d9f946e989cfd12e3b19abb654d31815
Animesh Yadav <animesh.yadav@tothenew.com>
2025-09-23 12:28:40 +0530

    added serializable annotation and organised imports accordinlgy

--- questionservice/src/main/java/com/ttn/questionservice/entities/Question.java
+++ questionservice/src/main/java/com/ttn/questionservice/entities/Question.java
@@ -7,13 +7,15 @@ import jakarta.persistence.Id;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serializable;
+
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Question {
+public class Question implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
